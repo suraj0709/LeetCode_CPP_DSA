8a90cdb3425db8896e988869fc0b9d629b3892ef
Suraj Kumar <43446354+suraj0709@users.noreply.github.com>
2024-07-22 20:34:11 +0530

    Time: 28 ms (24.16%), Space: 77.6 MB (7.77%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -9,47 +9,30 @@
  * };
  */
 class Solution {
-private:
-    void insertAtTail(ListNode* &head, ListNode* &tail, int digit){
-        ListNode* temp = new ListNode(digit);
-        
-        if(head == NULL){
-            head = temp;
-            tail = temp;
-        }else{
-            tail -> next = temp;
-            tail = temp;
-        }
-    }
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* head = NULL;
-        ListNode* tail = NULL;
-        
+        ListNode* dummy = new ListNode(-1);
+        ListNode* curr = dummy;
+        ListNode* t1 = l1;
+        ListNode* t2 = l2;
         int carry = 0;
         
-        while(l1 != NULL || l2 != NULL || carry != 0){
-            int val1 = 0;
-            if(l1 != NULL){
-                val1 = l1 -> val;
-                l1 = l1 -> next;
-            }
-            
-            int val2 = 0;
-            if(l2 != NULL){
-                val2 = l2 -> val;
-                l2 = l2 -> next;
-            }
-            
-            int sum  = val1 + val2 + carry;
-            int digit = sum % 10;
-            
-            insertAtTail(head, tail, digit);
-            
-            carry = sum / 10;            
+        while(t1 != NULL || t2 != NULL){
+            int sum = carry;
+            if(t1) sum += t1->val;
+            if(t2) sum += t2->val;
+            carry = sum / 10;
+            ListNode* temp = new ListNode(sum % 10);
+            curr->next = temp;
+            curr = curr->next;
+
+            if(t1) t1 = t1->next;
+            if(t2) t2 = t2->next;
         }
-        
-        return head;
-        
+        if(carry){
+            ListNode* temp = new ListNode(carry);
+            curr->next = temp;
+        }
+        return dummy->next;
     }
 };
